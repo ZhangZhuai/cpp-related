3b8e7c7ecccb64c1a98e2f3330868a4d1d053b06
zhuaizhang <zhuai_zhang@foxmail.com>
2022-05-27 22:36:39 +0800

    Modift istream implemetation in MyString

--- oop/MyString/MyString.cpp
+++ oop/MyString/MyString.cpp
@@ -1,35 +1,44 @@
 #include "MyString.h"
 
-MyString::MyString() : m_str {new char[1]} {
+MyString::MyString() : m_str{new char[1]}
+{
     m_str[0] = '\0';
 }
 
-MyString::MyString(char* str) {
-    if (str == nullptr) {
+MyString::MyString(char *str)
+{
+    if (str == nullptr)
+    {
         m_str = new char[1];
         m_str[0] = '\0';
-    } else {
+    }
+    else
+    {
         m_str = new char[strlen(str) + 1];
         strcpy(m_str, str);
     }
 }
 
-//Copy constructor
-MyString::MyString(const MyString& source) {
+// Copy constructor
+MyString::MyString(const MyString &source)
+{
     m_str = new char[strlen(source.m_str) + 1];
     strcpy(m_str, source.m_str);
     m_str[strlen(source.m_str)] = '\0';
 }
 
-//Move constructor
-MyString::MyString(MyString&& source) {
+// Move constructor
+MyString::MyString(MyString &&source)
+{
     m_str = source.m_str;
     source.m_str = nullptr;
 }
 
-//Overload the assignment operator
-MyString& MyString::operator=(const MyString& rhs) {
-    if (this == &rhs) return (*this);
+// Overload the assignment operator
+MyString &MyString::operator=(const MyString &rhs)
+{
+    if (this == &rhs)
+        return (*this);
     delete[] m_str;
     m_str = new char[strlen(rhs.m_str) + 1];
     strcpy(m_str, rhs.m_str);
@@ -37,20 +46,10 @@ MyString& MyString::operator=(const MyString& rhs) {
     return *this;
 }
 
-
-istream& operator>>(istream& is, MyString& obj) {
-    char* buff = new char[1000];
-    //memset warning
-    memset(&buff[0], 0, sizeof(buff));
-    is >> buff;
-    obj = MyString{buff};
-    delete[] buff;
-    return is;
-}
-
-MyString operator+(const MyString& lhs, const MyString& rhs) {
+MyString operator+(const MyString &lhs, const MyString &rhs)
+{
     int len = lhs.getSize() + rhs.getSize();
-    char* buff = new char[len + 1];
+    char *buff = new char[len + 1];
     strcpy(buff, lhs.m_str);
     strcat(buff, rhs.m_str);
     buff[len] = '\0';
@@ -61,23 +60,28 @@ MyString operator+(const MyString& lhs, const MyString& rhs) {
     return temp;
 }
 
-void MyString::swap(MyString& rhs) {
+void MyString::swap(MyString &rhs)
+{
     MyString tmp{rhs};
     rhs = *this;
     *this = tmp;
 }
 
-void MyString::copy(char s[], int len, int pos) {
-    for (int i = 0; i < len; ++i) {
+void MyString::copy(char s[], int len, int pos)
+{
+    for (int i = 0; i < len; ++i)
+    {
         s[i] = m_str[pos + 1];
     }
     s[len] = '\0';
 }
 
-void MyString::push_bk(char a) {
+void MyString::push_bk(char a)
+{
     int len = getSize();
-    char* buff = new char[len + 2];
-    for (int i = 0; i < len; ++i) {
+    char *buff = new char[len + 2];
+    for (int i = 0; i < len; ++i)
+    {
         buff[i] = m_str[i];
     }
     buff[len] = a;
@@ -87,10 +91,12 @@ void MyString::push_bk(char a) {
     delete[] buff;
 }
 
-void MyString::pop_bk() {
+void MyString::pop_bk()
+{
     int len = getSize();
-    char* buff = new char[len];
-    for (int i = 0; i < len - 1; ++i) {
+    char *buff = new char[len];
+    for (int i = 0; i < len - 1; ++i)
+    {
         buff[i] = m_str[i];
     }
     buff[len - 1] = '\0';
--- oop/MyString/MyString.h
+++ oop/MyString/MyString.h
@@ -2,26 +2,28 @@
 #define MY_STRING_H
 #include <iostream>
 #include <cstring>
-using std::ostream;
 using std::istream;
-class MyString {
-    //Prototype for stream insertion
-    friend ostream& operator<<(ostream& os, const MyString& obj) {
+using std::ostream;
+class MyString
+{
+    // Prototype for stream insertion
+    friend ostream &operator<<(ostream &os, const MyString &obj)
+    {
         os << obj.m_str;
         return os;
     }
-    
-    //Prototype for stream extraction
-    friend istream& operator<<(istream& is, const MyString& obj);
 
-    friend MyString operator+(const MyString& lhs, const MyString& rhs);
+    // Prototype for stream extraction
+    friend istream &operator<<(istream &is, const MyString &obj);
 
-    char* m_str;
+    friend MyString operator+(const MyString &lhs, const MyString &rhs);
+
+    char *m_str;
 
 public:
     MyString();
 
-    MyString(char* val);
+    MyString(char *val);
 
     size_t getSize() const { return strlen(m_str); }
 
@@ -31,19 +33,29 @@ public:
 
     void copy(char s[], int len, int pos);
 
-    void swap(MyString& rhs);
-    
-    //Copy constructor
-    MyString(const MyString& source);
+    void swap(MyString &rhs);
 
-    //Move constructor
-    MyString(MyString&& source);
+    // Copy constructor
+    MyString(const MyString &source);
 
-    //Overload the assignment operator
-    MyString& operator=(const MyString& rhs);
+    // Move constructor
+    MyString(MyString &&source);
+
+    // Overload the assignment operator
+    MyString &operator=(const MyString &rhs);
 
     ~MyString() { delete[] m_str; }
 };
 
+istream &operator>>(istream &is, MyString &obj)
+{
+    char *buff = new char[1000];
+    // memset warning
+    memset(&buff[0], 0, sizeof(buff));
+    is >> buff;
+    obj = MyString{buff};
+    delete[] buff;
+    return is;
+}
 
 #endif // MyString
